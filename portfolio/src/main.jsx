cf07a9036b6e4d4d9e9b45adc64b4db6b34bc6c3
Jhilik1019 <jhilikb894@gmail.com>
2025-01-17 10:54:55 +0530

    Comment

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -1,13 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'; // Adjust if your App component is in a different path
+import './index.css'; // Make sure to import your styles
 
-//import "@fontsource/outfit";
-//import "@fontsource/roboto";
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
